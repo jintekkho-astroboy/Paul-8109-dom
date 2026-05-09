cb23d8638ad12a21c8ad8b72206482b781741160
jintekkho-astro <jintek.kho@gmail.com>
2026-05-09 17:02:15 +0800

    added ol in 03-create-element html for testing purpose

--- 03-create-element/script.js
+++ 03-create-element/script.js
@@ -4,9 +4,15 @@ document.querySelector("#addBtn").addEventListener("click", function(){
     // document.createElement allows us to create a new DOM
     // element
     const newListItem = document.createElement("li");
-    newListItem.innerHTML = `${newNumber} <button>Say Hi</button>`
+    newListItem.innerHTML = `${newNumber} <button>Say Hi</button>`;
 
     // get the parent of the new list item
     const parent = document.querySelector("#numberList");
     parent.appendChild(newListItem);
+
+    // create element for ordered list
+    const newOlItem = document.createElement("ol");
+    newOlItem.innerText = newNumber;
+    const olParent = document.querySelector("#numberOl");
+    olParent.appendChild(newOlItem);
 })
